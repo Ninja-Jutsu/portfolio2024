19d3b80380b26b257668df06e91c7d77b461a816
ismail-bardach <ismail.bardach.work@gmail.com>
2024-08-11 05:09:33 +0800

    Build About Section

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 const SectionTitle = ({ text }) => {
   return (
-    <div className='border-b border-gray-200 pb-5'>
+    <div className='border-b border-gray-300 pb-5'>
       <h2 className='text-3xl font-medium tracking-wider capitalize'>{text}</h2>
     </div>
   )

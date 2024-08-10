3acf7c0bf1b6301cb787e0ce76a04c76d158f409
ismail-bardach <ismail.bardach.work@gmail.com>
2024-08-11 05:10:26 +0800

    Assemble App file & edit global css bg color

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import Hero from './components/Hero'
 import NavBar from './components/NavBar'
+import Skills from './components/Skills'
+import About from './components/About'
+import Projects from './components/Projects'
 
 function App() {
   return (
     <>
       <NavBar />
       <Hero />
+      <Skills />
+      <About />
+      <Projects />
     </>
   )
 }

ea6e41d2b147432d71e0f07fc8f1f902edc5f733
Ninja-Jutsu <ninja.jutsu.way@gmail.com>
2024-09-12 04:54:08 +0800

    Reworked ABout Section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,11 @@
-import aboutSvg from '../assets/about.jpg'
+import aboutSvg from '../assets/ismail-photo.jpg'
 import { GlareCard } from './ui/glare-card'
 import SectionTitle from './SectionTitle'
 import { languages } from '../data'
 const About = () => {
   return (
     <section
-      className='mt-0 p-10 sm:-mt-10 sm:py-10'
+      className='mt-0 p-10 pb-0 sm:-mt-10 sm:py-10'
       id='about'
     >
       <div className='align-element px-8 mb-5'>
@@ -17,7 +17,7 @@ const About = () => {
           <GlareCard>
             <img
               src={aboutSvg}
-              className='w-[400px] h-[500px] object-cover'
+              className='w-[500px] h-[600px] object-cover'
             />
           </GlareCard>
         </div>
@@ -27,25 +27,45 @@ const About = () => {
           className='w-full h-full rounded-full sm:hidden'
         />
         <article className='-ml-5'>
-          <p className='text-stone-900 text-l mt-8 leading-loose md:2xl lg:text-2xl tracking-wide'>
-            Hi there, this is Ismail, a 30-year-old web developer making waves in the digital landscape of Singapore. My
-            toolkit includes
-            <span className='text-stone-800 font-poppins italic'> HTML, CSS, JavaScript</span> and much more, but my
-            real superpower lies in turning concepts into captivating experiences.
-            <ul className='mt-5'>
-              <li className='mb-2'>
-                - I have a strong foundation in JavaScript and am skilled in utilizing React  in addition to the new Next.js framework to develop
-                robust web applications.
-              </li>
-              <li className='mb-2'>
-                - I possess a deep understanding and proficiency in database systems, including MySQL and MongoDB.
-              </li>
-              <li className='mb-2'>
-                - Driven by my passion for JavaScript, I utilize Node.js to create scalable and efficient backend
-                architectures.
-              </li>
-            </ul>
-          </p>
+          <div className='text-stone-900 text-l mt-8 leading-loose md:2xl lg:text-2xl tracking-wide flex flex-col gap-4'>
+            <p>
+              As a seasoned web developer based in Singapore, I specialize in crafting engaging and user-centric digital
+              experiences. I&apos;m passionate about transforming ideas into reality, leveraging my skills to build
+              robust and scalable web applications.
+            </p>
+            <p>
+              My technical expertise encompasses a range of front-end and back-end technologies, including{' '}
+              <span className='text-blue-600'>JavaScript, React, Next.js, Node.js, MySQL, and MongoDB.</span>
+            </p>
+            <div className='mt-5'>
+              <h4 className='mb-2'>
+                In addition to my technical proficiency, I bring a strong set of soft skills to every project,
+                including:
+              </h4>
+              <ul className='ml-4'>
+                <li className='mb-2'>
+                  - <strong>Problem-Solving:</strong> I thrive on tackling complex challenges and devising creative
+                  solutions.
+                </li>
+                <li className='mb-2'>
+                  - <strong>Communication:</strong>I excel at collaborating with team members and stakeholders, ensuring
+                  clear and effective communication throughout the development process.
+                </li>
+                <li className='mb-2'>
+                  - <strong>Adaptability:</strong> The ever-evolving tech landscape demands flexibility, and I&apos;m
+                  adept at learning new technologies and adapting to changing project requirements.
+                </li>
+                <li className='mb-2'>
+                  - <strong>Time Management:</strong> I prioritize tasks effectively, ensuring timely project delivery
+                  without compromising quality.
+                </li>
+                <li className='mb-2'>
+                  <strong>Attention to Detail:</strong> I&apos;m meticulous in my work, ensuring that every line of code
+                  is clean, efficient, and error-free.
+                </li>
+              </ul>
+            </div>
+          </div>
           <h2 className='text-stone-900 leading-loose lg:text-2xl tracking-wide mt-8 mb-4 font-poppins'>I speak:</h2>
           <div className='flex flex-row items-center justify-start mb-10 w-full gap-4 sm:gap-10'>
             {languages.map((language) => {
